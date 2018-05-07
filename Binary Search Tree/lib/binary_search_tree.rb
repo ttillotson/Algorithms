9292dddd65c96b2b866c93910e79423934b92052
Travis Tillotson <travis.don.tillotson@gmail.com>
2018-05-07 00:06:40 -0700

    BST Delete

--- Binary Search Tree/lib/binary_search_tree.rb
+++ Binary Search Tree/lib/binary_search_tree.rb
@@ -52,10 +52,55 @@ class BinarySearchTree
   end
 
   def delete(value)
+    node = find(value)
+    return nil unless node 
+
+    # If desired node has 2 children
+    if node.left && node.right 
+      # Find largest node in left tree
+      rep_node = maximum(node.left) 
+
+      # Transition Replacement Out (rep_node)
+      # Assign pointers to former parent/left-child
+      rep_node.parent.right = rep_node.left
+      rep_node.left.parent = rep_node.parent
+
+      # Transition Replacement Into Deleted's Spot
+      # Assign Node's children to Replacement
+      rep_node.left = node.left 
+      rep_node.left.parent = rep_node
+      rep_node.right = node.right
+      rep_node.right.parent = rep_node
+
+      # Tell the parent it has a new child
+      node.parent.left = rep_node if node.parent.left = node
+      node.parent.right = rep_node if node.parent.right = node
+    elsif node.left || node.right
+      rep_node = node.left
+      rep_node ||= node.right 
+
+      rep_node.left = node.left 
+      rep_node.left.parent = rep_node if rep_node.left
+      rep_node.right = node.right
+      rep_node.right.parent = rep_node if rep_node.right
+
+      # Tell the parent it has a new child
+      node.parent.left = rep_node if node.parent.left = node
+      node.parent.right = rep_node if node.parent.right = node
+    else # No Children
+      @root = nil unless node.parent
+
+      if node.parent
+        node.parent.left = nil if node.parent.left = node
+        node.parent.right = nil if node.parent.right = node
+      end
+    end
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    return maximum(tree_node.right) if tree_node.right 
+    tree_node
   end
 
   def depth(tree_node = @root)
@@ -70,5 +115,12 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
+  def reassign_children(old_node, new_node=nil)
 
+  end
+
+  def reassign_parent(old_node, new_node=nil)
+    node.parent.left = new_node if node.parent.left = node
+    node.parent.right = new_node if node.parent.right = node
+  end
 end
