fe878da3ed05efa182a9b17e03539d54875849e4
Travis Tillotson <travis.don.tillotson@gmail.com>
2018-05-06 23:02:38 -0700

    Insert BST

--- Binary Search Tree/lib/binary_search_tree.rb
+++ Binary Search Tree/lib/binary_search_tree.rb
@@ -1,15 +1,44 @@
+require_relative 'bst_node'
 # There are many ways to implement these methods, feel free to add arguments 
 # to methods as you see fit, or to create helper methods.
 
 class BinarySearchTree
+  attr_reader :root
   def initialize
-    
+    @root = nil
   end
 
-  def insert(value)
+  def insert(value, parent = @root)
+    unless @root 
+      @root = BSTNode.new(value)
+      @root.parent = nil
+      return
+    end
+
+    if value <= parent.value 
+      if parent.left
+        insert(value, parent.left) 
+      else
+        node = BSTNode.new(value)
+        parent.left = node
+        node.parent = parent
+      end
+    end
+
+    if value > parent.value 
+      if parent.right
+        insert(value, parent.right) 
+      else
+        node = BSTNode.new(value)
+        parent.right = node
+        node.parent = parent
+      end
+    end
+
   end
 
   def find(value, tree_node = @root)
+
   end
 
   def delete(value)
